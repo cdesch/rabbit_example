3e25dc13ad56bc1a622baf085b73873e0b3abefd
Chris <cdesch@gmail.com>
2021-12-05 22:55:49 -0500

    initial commit

---
+++ lib/rabbit_example.ex
@@ -0,0 +1,18 @@
+defmodule RabbitExample do
+  @moduledoc """
+  Documentation for `RabbitExample`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RabbitExample.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/rabbit_example/application.ex
@@ -0,0 +1,20 @@
+defmodule RabbitExample.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: RabbitExample.Worker.start_link(arg)
+      # {RabbitExample.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: RabbitExample.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
